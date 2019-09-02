703706a51ba101e7cb766dbe1f78fb482681d28b
Diedre Brown <diedre@diedrebrown.com>
2019-09-02 19:10:58 -0400

    Add files via upload
    Introduction lab

---
+++ Introduction.R
@@ -0,0 +1,19 @@
+#Pratt INFO 640 Fall 2019
+#Lab 1 Introduction to R
+#Code by Diedre Brown; dbrow207@pratt.edu
+
+my_num <- 1
+my_char<-"hi"
+my_bool<-TRUE
+
+typeof(my_num)
+typeof(my_char)
+typeof(my_bool)
+
+#creates a null variable
+my_nan<- 0/0
+my_nan
+typeof(my_nan)
+
+install.packages("tidyverse")
+library(tidyverse)
